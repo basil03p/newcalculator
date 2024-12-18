0f235e5e0b870a6649b902f90a4f3766316020fe
basil03p <basilmalikudy3@gmail.com>
2024-12-18 11:59:53 +0530

    addition completed

--- app/src/main/java/com/suii/newcalculator/MainActivity.java
+++ app/src/main/java/com/suii/newcalculator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.suii.newcalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +12,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b1,b2,b3,b4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        b1=(Button) findViewById(R.id.plus);
+        b2=(Button) findViewById(R.id.min);
+        b3=(Button) findViewById(R.id.mul);
+        b4=(Button) findViewById(R.id.div);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), add.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
